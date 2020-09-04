28718c49efb897ca1bcaea92f359e307a9ddf7b6
Nhan Nguyen <a4rum.vn@gmail.com>
2020-09-04 15:06:25 +0700

    Fix router for index

--- internal/interface/router.go
+++ internal/interface/router.go
@@ -39,7 +39,7 @@ func (r *router) Setup() {
 			// Set the HTTP status to 200 (OK)
 			http.StatusOK,
 			// Use the index.html template
-			"index.html",
+			"index.tmpl",
 			// Pass the data that the page uses (in this case, 'title')
 			gin.H{
 				"title": "Nguyen Van Trong Nhan",
